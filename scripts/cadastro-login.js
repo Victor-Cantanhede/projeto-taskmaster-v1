1275526a51222b38e63dbfca2ee9d392ae45ef76
Victor-Cantanhede <victorcantanhedblk@gmail.com>
2024-10-14 08:20:54 -0300

    Regra nome de usu ok

--- scripts/cadastro-login.js
+++ scripts/cadastro-login.js
@@ -1,7 +1,5 @@
 //////////////////////////////////////////////////////////////////////
 
-/*SCRIPT PARA VALIDAÇÃO DE CADASTRO PESSOA FÍSICA*/
-
 // VARIÁVEIS
 const cpf = document.getElementById('icpf');
 const nomeCompleto = document.getElementById('inome-completo');
@@ -13,37 +11,114 @@ const formValidatePJ = document.getElementById('form-pessoa-juridica');
 const btnPessoal = document.getElementById('label-perfil-pessoal');
 const btnEmpresarial = document.getElementById('label-perfil-empresarial');
 
-// BLOQUEANDO CARACTERES NÃO NUMÉRICOS
-cpf.addEventListener('input', function (event) {
+const campoUsuarioPF = document.getElementById('iuser');
+const campoSenhaPF = document.getElementById('ipassword');
+const campoConfirmSenhaPF = document.getElementById('ipassword002');
+
+//////////////////////////////////////////////////////////////////////
+
+/*SCRIPT PARA VALIDAÇÃO DE CADASTRO PESSOA FÍSICA*/
+
+
+// BLOQUEANDO CARACTERES NÃO NUMÉRICOS NO CAMPO CPF E TELEFONE
+cpf.addEventListener('input', function() {
     this.value = this.value.replace(/[^0-9]/g, '');
 });
 
-telefone.addEventListener('input', function (event) {
+telefone.addEventListener('input', function() {
     this.value = this.value.replace(/[^0-9]/g, '');
 });
 
-// LIMITANDO QUANTIDADE DE CARACTERES
-formValidatePF.addEventListener('submit', function (event) {
-    if (formCpf.value.length !== 11) {
+// CONVERTENDO CARACTERES PARA LETRAS MAIÚSCULAS NO CAMPO NOME COMPLETO
+nomeCompleto.addEventListener('input', function() {
+    this.value = this.value.toUpperCase().replace(/[0-9]/g, '');
+});
+
+// VALIDANDO DADOS PARA ENVIO DO FORMULÁRIO
+formValidatePF.addEventListener('submit', function(event) {
+
+    // LIMITANDO QUANTIDADE DE CARACTERES NO CAMPO CPF
+    if (cpf.value.length !== 11) {
         event.preventDefault();
-        formCpf.value = '';
+        cpf.value = '';
         window.alert('CPF inválido, tente novamente!');
     }
-});
 
-// CONVERTENDO CARACTERES PARA LETRAS MAIÚSCULAS
-nomeCompleto.addEventListener('input', function() {
-    this.value = this.value.toUpperCase();
-});
-
-// REMOVENDO ESPAÇOS EM BRANCO ENTRE CARACTERES
-function removeSpaces(text) {
-    return text.replace(/\s+/g, ' ').trim();
-}
-
-formValidatePF.addEventListener('submit', function() {
+    // REMOVENDO ESPAÇOS EM BRANCO ENTRE CARACTERES NO CAMPO NOME COMPLETO
+    function removeSpaces(text) {
+        return text.replace(/\s+/g, ' ').trim();
+    }
     nomeCompleto.value = removeSpaces(nomeCompleto.value);
+    
+    // REGRAS PARA CRIAÇÃO DO NOME DE USUÁRIO
+    /*
+    >> Deve conter no mínimo 10 caracteres e no máximo 20 caracteres
+    >> Deve conter ao menos 1 letra maiúscula e ao menos 1 letra minúscula
+    >> Deve conter ao menos 1 número
+    >> Pode conter caracteres como "." ou "-" ou "_";
+    >> Não pode conter caracteres especiais (exemplo: !, @, #, $, %)
+    */
+    
+    // VERIFICANDO QUANTIDADE DE CARACTERES DO NOME DE USUÁRIO
+    if (campoUsuarioPF.value.length >= 10 && campoUsuarioPF.value.length <= 20) {
+        console.log('Nome de usuário: Quantidade de caracteres ok!');
+
+        // VERIFICANDO SE O NOME DE USUÁRIO POSSUI ALGUMA LETRA MINÚSCULAS E MAIÚSCULAS
+        const usuarioPossuiMinuscula = /[a-z]/.test(campoUsuarioPF.value);
+        const usuarioPossuiMaiuscula = /[A-Z]/.test(campoUsuarioPF.value);
+
+        if (usuarioPossuiMinuscula && usuarioPossuiMaiuscula) {
+            console.log('Nome de usuário: Possui letras minúsculas e maiúsculas ok!');
+
+            // VERIFICANDO SE O NOME DE USUÁRIO POSSUI ALGUM NÚMERO
+            const usuarioPossuiNumero = /[0-9]/.test(campoUsuarioPF.value);
+
+            if (usuarioPossuiNumero) {
+                console.log('Nome de usuário: Possui números ok!');
+
+                // VERIFICANDO SE O NOME DE USUÁRIO POSSUI CARACTERES ESPECIAIS
+                const PossuiCaracteresEsp = /[^a-zA-Z0-9._-]/.test(campoUsuarioPF.value);
+
+                if (!PossuiCaracteresEsp) {
+                    console.log('Nome de usuário: Não possui caracteres especiais ok!');
+
+                    // FIM DA VERIFICAÇÃO DO NOME DE USUÁRIO
+
+                } else {
+                    event.preventDefault();
+                    campoUsuarioPF.value = '';
+                    console.log('ERRO: O nome de usuário possui caracteres especiais!');
+                    window.alert('ERRO: O nome de usuário não deve possuir caracteres especiais');
+                }
+
+            } else {
+                event.preventDefault();
+                campoUsuarioPF.value = '';
+                console.log('ERRO: O nome de usuário não possui números!');
+                window.alert('ERRO: O nome de usuário deve possuir pelo menos um número!');
+            }
+
+        } else {
+            event.preventDefault();
+            campoUsuarioPF.value = '';
+            console.log('ERRO: O nome de usuário deve possuir pelo menos uma letra minúscula e maiúscula!');
+            window.alert('ERRO: O nome de usuário deve possuir pelo menos uma letra minúscula e maiúscula!');
+        }
+
+    } else {
+        event.preventDefault();
+        campoUsuarioPF.value = '';
+        console.log('ERRO: Quantidade de caracteres do nome de usuário inválida!');
+        window.alert('ERRO: O nome de usuário deve possuir de 10 a 20 caracteres!');
+    }
+
+    // REGRAS PARA CRIAÇÃO DA SENHA
+    /*
+    
+    */
 });
+
+
 
 //////////////////////////////////////////////////////////////////////
 
